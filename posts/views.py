bce3a290654f46f9d61baaff24c0107957b9f78b
SeolHuiGwan9478 <shg5977@naver.com>
2021-10-01 21:07:01 +0900

    modify views.py logic

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
 from .forms import PostForm
 # Create your views here.
@@ -13,5 +13,16 @@ def post_detail(request, post_id):
     return render(request, 'posts/post_detail.html', context=context)
 
 def post_create(request):
-    post_form = PostForm()
-    return render(request, 'posts/post_form.html', {'form': post_form})
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        new_post = Post(
+            title = title,
+            content = content
+            )
+        new_post.save()
+        return redirect('post-detail', post_id=new_post.id)
+    
+    else:
+        post_form = PostForm()
+        return render(request, 'posts/post_form.html', {'form': post_form})
