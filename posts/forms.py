e6cac1cf8364d8850953bbf5c93d9470f0832bd6
SeolHuiGwan9478 <shg5977@naver.com>
2022-01-05 03:13:52 +0900

    learning cleaned_data

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Post
+from django.core.exceptions import ValidationError
 class PostForm(forms.ModelForm):
     
     class Meta:
         model = Post
         fields = ['title', 'content']
+    
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if '*' in title:
+            raise ValidationError("'*'은 포함될 수 없습니다.")
+        return title
