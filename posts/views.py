04bad9641b74983cd9bb56c9b8d7977a556c8e40
SeolHuiGwan9478 <shg5977@naver.com>
2022-01-08 00:43:51 +0900

    FBV->CBV

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from .models import Post
 from .forms import PostForm
 from django.core.paginator import Paginator
+from django.views import View
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
@@ -18,15 +19,18 @@ def post_detail(request, post_id):
     context = {"post":post}
     return render(request, 'posts/post_detail.html', context=context)
 
-def post_create(request):
-    if request.method == 'POST':
-        post_form = PostForm(request.POST)
+class PostCreateView(View):
+    # method GET
+    def get(self, request):
+        post_form = PostForm()
+        return render(request, 'posts/post_form.html', {'form': post_form})
+
+    def post(self, request):
+        post_form = PostForm(reqeust.POST)
         if post_form.is_valid():
             new_post = post_form.save()
-            return redirect('post-detail', post_id=new_post.id)
-    else:
-        post_form = PostForm()
-    return render(request, 'posts/post_form.html', {'form': post_form})
+            return redirect('post-detail', post_id = new_post.id)
+        return render(request, 'posts/post_form.html', {'form':post_form})
 
 def post_update(request, post_id):
     post = get_object_or_404(Post, id=post_id)
