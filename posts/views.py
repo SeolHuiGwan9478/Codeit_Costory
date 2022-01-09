04d98c0617ca66de2ac82f3a2cd1f1a315de9414
SeolHuiGwan9478 <shg5977@naver.com>
2022-01-09 21:11:09 +0900

    simplify views.py by using generic view

--- posts/views.py
+++ posts/views.py
@@ -3,48 +3,44 @@ from django.http import Http404
 from .models import Post
 from .forms import PostForm
 from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView, ListView, DetailView, UpdateView, DeleteView, RedirectView
+)
 from django.urls import reverse
 #from django.views import View
 class PostListView(ListView):
     model = Post
-    template_name = 'posts/post_list.html'
-    context_object_name = 'posts'
+    #template_name = 'posts/post_list.html'
+    #context_object_name = 'posts'
     ordering = ['-dt_created']
     paginate_by = 6
-    page_kwarg = 'page'
+    #page_kwarg = 'page'
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'posts/post_detail.html'
-    context_object_name = 'post'
-    pk_url_kwarg = 'post_id'    
+    #template_name = 'posts/post_detail.html'
+    #context_object_name = 'post'
+    #pk_url_kwarg = 'post_id'    
 
 class PostCreateView(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'posts/post_list.html'
 
     def get_success_url(self):
-        return reverse('post-detail', kwargs={'post_id':self.object.id})
+        return reverse('post-detail', kwargs={'pk':self.object.id})
 
 class PostUpdateView(UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'posts/posts_form.html'
-    pk_url_kwarg = 'post_id'
 
     def get_success_url(self):
-        return reverse('post_detail', kwarg={'post_id': self.object.id})
+        return reverse('post_detail', kwarg={'pk': self.object.id})
 
 class PostDeleteView(DeleteView):
     model = Post
-    template_name = 'posts/post_confirm_delete.html'
-    pk_url_kwarg = 'post_id'
-    context_object_name = 'post'
 
     def get_success_url(self):
         return reverse('post-list')
     
-def index(request):
-    return redirect('post-list')
+class IndexRedirectView(RedirectView):
+    pattern_name = 'post-list'
