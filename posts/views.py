94bc80dac253a7305b69addc2e81846274687b79
SeolHuiGwan9478 <shg5977@naver.com>
2022-01-09 20:26:26 +0900

    modify PostUpdateView, PostDeleteView

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from .models import Post
 from .forms import PostForm
 from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse
 #from django.views import View
 class PostListView(ListView):
@@ -28,24 +28,23 @@ class PostCreateView(CreateView):
     def get_success_url(self):
         return reverse('post-detail', kwargs={'post_id':self.object.id})
 
-def post_update(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == 'POST':
-        post_form = PostForm(request.POST, instance=post)
-        if post_form.is_valid():
-            post_form.save()
-            return redirect('post-detail', post.id)
-    else:
-        post_form = PostForm(instance=post)
-    return render(request, 'posts/post_form.html', {'form': post_form})
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/posts_form.html'
+    pk_url_kwarg = 'post_id'
 
-def post_delete(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == "POST":   
-        post.delete()
-        return redirect('post-list')
-    else:
-        return render(request, 'posts/post_confirm_delete.html', {'post':post})
+    def get_success_url(self):
+        return reverse('post_detail', kwarg={'post_id': self.object.id})
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'posts/post_confirm_delete.html'
+    pk_url_kwarg = 'post_id'
+    context_object_name = 'post'
+
+    def get_success_url(self):
+        return reverse('post-list')
     
 def index(request):
     return redirect('post-list')
